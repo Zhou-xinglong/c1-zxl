2c91410e23e50a53c7fc4b30ae28480f6e05336c
世纪末 <2967803226@qq.com>
2021-01-30 14:51:08 +0800

    第一周第六题

--- week1/src/task6.java
+++ week1/src/task6.java
@@ -1,22 +1,44 @@
 import java.util.Scanner;
 public class task6 {
-    String name;
-    double money;
-    void Kh()
-    {
-        Scanner in = new Scanner(System.in);
-        System.out.print("请输入你的账户名字：");
-        name=in.nextLine();
-        System.out.print("请输入你要存的钱：");
-        money=in.nextDouble();
-    }
-    void showMoney()
-    {
-        System.out.println("你的余额为："+money);
-    }
     public static void main(String[] args) {
-        task6 v = new task6();
-        v.Kh();
-        v.showMoney();
+        Scanner in = new Scanner(System.in);
+        int n;
+        System.out.println("请选择你要进行的操作(1是开户，查询余额）：");
+        n=in.nextInt();
+        Bank s = new Bank(n);
+        if(n==1)
+        {
+            s.ku();
+        }
+        else
+        {
+            s.Cx();
+        }
+    }
+}
+class Bank
+{
+    String name;
+    double ye ;
+    Scanner in =new Scanner(System.in);
+    double ku()
+    {
+        System.out.print("请输入你的账户名称:");
+        name = in.nextLine();
+        double money;
+        System.out.print("请输入你要存入钱：");
+        money = in.nextDouble();
+        return money;
+    }
+    Bank(double ye)
+    {
+        this.ye=ye;
+    }
+    void Cx()
+    {
+        System.out.print("请输入你的名字：");
+        name=in.nextLine();
+
+        System.out.println("你的余额为："+ye);
     }
 }
--- week1/src/task7.java
+++ week1/src/task7.java
@@ -2,6 +2,38 @@ import java.util.Scanner;
 public class task7 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        int a1,b1,a2,b2;
+        System.out.print("输入第一个复数的实部和虚部：");
+        a1=in.nextInt();
+        b1=in.nextInt();
+        System.out.print("输入第二个复数的实部和虚部：");
+        a2= in.nextInt();
+        b2=in.nextInt();
+        fs s = new fs();
+        s.add(a1,a2,b1,b2);
+        s.reduce(a1,a2,b1,b2);
+        s.multiply(a1,a2,b1,b2);
+    }
+}
+class fs
+{
+    int a1,a2,b1,b2;
+    void add(int a1,int a2,int b1,int b2)
+    {
+        int a3=a1+a2;
+        int b3=b1+b2;
+        System.out.println("两复数相加的结果为："+a3+"+"+b3+"i");
+    }
+    void reduce(int a1,int a2,int b1,int b2)
+    {
+        int a3=a1-a2;
+        int b3=b1-b2;
+        System.out.println("两复数相减的结果为："+a3+"+"+b3+"i");
+    }
+    void multiply(int a1,int a2,int b1,int b2)
+    {
+        int a3=a1*a2-b1*b2;
+        int b3=a1*b2+a2*b1;
+        System.out.println("两复数相乘的结果为："+a3+"+"+b3+"i");
     }
 }
