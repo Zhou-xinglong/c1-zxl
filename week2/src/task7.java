146b8206069ff7fc9aa9fb1f4d759557a0706475
世纪末 <2967803226@qq.com>
2021-02-08 11:17:19 +0800

    越来越搞不懂了

---
+++ week2/src/task7.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class task7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Poultry s =new Poultry();
+        s.showMsg();
+        s.setIllness();
+        s.showSymptom();
+    }
+}
+class Poultry
+{
+    String name,symptom,illness;
+    int age;
+    Poultry()
+    {
+        this("鸭子","发烧","感冒",2);
+    }
+    Poultry(String name,String sympthom, String illness,int age)
+    {
+        this.name=name;
+        this.symptom=sympthom;
+        this.illness=illness;
+        this.age=age;
+    }
+    public void showMsg()
+   {
+        System.out.println("动物种类:"+name+",年龄："+age+"岁");
+    }
+    public void setIllness()
+    {
+        System.out.println("入院原因:"+illness);
+    }
+    public void showSymptom()
+    {
+        System.out.println("症状为:"+symptom);
+    }
+}
+class Duck extends Poultry
+{
+
+}
---
+++ week2/src/task8.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class task8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
+class A
+{
+    int numa=10;
+    public void showA()
+    {
+        System.out.println("A类中的numa："+numa);
+    }
+}
+class B
+{
+    int numb=20;
+    public void showB()
+    {
+        System.out.println("B类中的numb："+numb);
+    }
+}
