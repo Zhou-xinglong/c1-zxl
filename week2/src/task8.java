2ffca19c4c8c1fac72f20fec5b6f0f623a238787
世纪末 <2967803226@qq.com>
2021-02-08 16:15:30 +0800

    第8题

--- week2/src/task8.java
+++ week2/src/task8.java
@@ -3,22 +3,33 @@ import java.util.Scanner;
 public class task8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        C c = new C();
+        c.showA();
+        c.showB();
+        c.showC();
     }
 }
 class A
 {
-    int numa=10;
+    private int numa=10;
     public void showA()
     {
         System.out.println("A类中的numa："+numa);
     }
 }
-class B
+class B extends A
 {
-    int numb=20;
+    private int numb=20;
     public void showB()
     {
         System.out.println("B类中的numb："+numb);
+    }
+}
+class C extends B
+{
+    private int numc=30;
+    public void showC()
+    {
+        System.out.println("C类中的numc："+numc);
     }
 }
