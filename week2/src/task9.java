5866f5918e62c20e36fb668adf1bafcc9bf7bf6e
世纪末 <2967803226@qq.com>
2021-02-10 11:01:34 +0800

    第9题

--- week2/src/task9.java
+++ week2/src/task9.java
@@ -1,28 +1,25 @@
 public class task9 {
     public static void main(String[] args) {
-            Star c1  = new Star();
-            c1.shine();
-            Sun c2 =new Sun();
-            c2.doAnything();
-            Star a = new Sun();
-            Sun c3 = (Sun)a;
-            c3.shine();
-    }
-}
-
+        Star star = new Star();
+        star.shine();
+        System.out.println("====================");
+        Universe universe = new Sun();
+        universe.doAnything();
+        Sun sun = (Sun) universe;
+        sun.shine();
+    } }
 interface Universe{
     public abstract void doAnything();
 }
-class Star{
+class Star {
     public void shine(){
-        System.out.println("star: 星星一闪一闪亮晶晶");
-        System.out.println("====================");
-    }
-}
+        System.out.println("star:星星一闪一闪亮晶晶");
+    } }
 class Sun extends Star implements Universe{
-    public void doAnything(){
-        System.out.println("sun:太阳吸引着9大行星旋转");
-        System.out.println("sun:光照八分钟,到达地球");
+    public void doAnything() {
+        System.out.println("sun:太阳吸引着 9 大行星旋转");
     }
-}
+    public void shine() {
+        System.out.println("sun:光照八分钟,到达地球");
+    } }
 
